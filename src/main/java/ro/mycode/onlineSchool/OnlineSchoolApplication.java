d2c8cd58818f9dea98904ecd861c41f966fb64da
Valentin <vflorea97@yahoo.com>
2023-03-26 12:43:02 +0300

    create

---
+++ src/main/java/ro/mycode/onlineSchool/OnlineSchoolApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.onlineSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/onlineSchool/OnlineSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.onlineSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
